84124a4b366088e870447380eef88300b5b861c1
SloshyOsmosis <dherbert5229@gmail.com>
2024-03-26 13:36:25 +0000

    Implemented Adi's login/signup Including images

--- app/src/main/java/com/example/myapplication/MyAdapter.java
+++ app/src/main/java/com/example/myapplication/MyAdapter.java
@@ -4,7 +4,9 @@ import android.content.ClipData;
 import android.content.Context;
 import android.media.RouteListingPreference;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,32 +14,56 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MyAdapter extends RecyclerView.Adapter<ViewHolder> {
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     Context context;
-    List<Cards> cards;
     List<Cards> filteredCards;
-    public MyAdapter(Context context, List<Cards> cards){
+
+    public MyAdapter(Context context, List<Cards> cards) {
         this.context = context;
-        this.cards = cards;
         this.filteredCards = new ArrayList<>(cards);
     }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.card_view,parent,false));
+        View itemView = LayoutInflater.from(context).inflate(R.layout.card_view, parent, false);
+        return new ViewHolder(itemView);
     }
+
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.imageView.setImageResource(filteredCards.get(position).getImage());
     }
+
     @Override
     public int getItemCount() {
         return filteredCards.size();
     }
 
-    public void filterList(List<Cards> filteredList){
+    public void filterList(List<Cards> filteredList) {
         filteredCards.clear();
         filteredCards.addAll(filteredList);
         notifyDataSetChanged();
+    }
+
+    // ViewHolder class
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        ImageView imageView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.cardView);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            if (position != RecyclerView.NO_POSITION) {
+                // Handle click event here
+                Cards clickedCard = filteredCards.get(position);
+                // You can perform actions such as starting a new activity here
+            }
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/ViewHolder.java
+++ app/src/main/java/com/example/myapplication/ViewHolder.java
@@ -1,15 +1,30 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ViewHolder extends RecyclerView.ViewHolder {
+import java.util.List;
+
+public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    Context context;
+    List<Cards> cards;
+    List<Cards> filteredCards;
     ImageView imageView;
     public ViewHolder(@NonNull View itemView){
         super(itemView);
         imageView = itemView.findViewById(R.id.cardView);
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int position = getAdapterPosition();
+        if (position!= RecyclerView.NO_POSITION){
+            Cards clickedCard = filteredCards.get(position);
+        }
     }
 }
